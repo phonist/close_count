0834c009f658f874bc218c6aa6cf56434488cfd0
BuzzerByte <rujyi94@hotmail.com>
2021-12-07 09:19:44 +0800

    update production

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
@@ -7,6 +8,13 @@ const app = express();
 // Connect Database
 connectDB();
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Init Middleware
 app.use(express.json());
 
@@ -15,6 +23,12 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/timers',require('./routes/api/timers'));
+
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
